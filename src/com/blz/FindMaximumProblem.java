9f50b0188ccc29e2eea1b088ede443f062a31c92
Priyanka <priyankadombale27@gmail.com>
2022-03-01 12:30:41 +0530

    Added : Find maximum in String

--- src/com/blz/FindMaximumProblem.java
+++ src/com/blz/FindMaximumProblem.java
@@ -6,8 +6,8 @@ public class FindMaximumProblem {
 	 * Extend the max method to also print the max to std out using Generic Method
 	 */
 
-	public static Float maxFloat(Float a, Float b, Float c) {
-		Float max = a;// assume a is initially largest
+	public static String maxString(String a, String b, String c) {
+		String max = a;// assume a is initially largest
 		if (b.compareTo(max) > 0)
 			max = b;
 		if (c.compareTo(max) > 0)
@@ -23,7 +23,7 @@ public class FindMaximumProblem {
 	// main method
 	public static void main(String[] args) {
 		System.out.println("Welcome..!!");
-		// Calling method with Float argument
-		FindMaximumProblem.maxFloat(7.12f, 10.32f, 9.45f);
+		// Calling method with String argument
+		FindMaximumProblem.maxString("Priya", "Anu", "Sonu");
 	}
 }
