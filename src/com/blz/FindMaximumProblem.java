be790e545a2940cda988dccad0174e88dd98ee1f
Priyanka <priyankadombale27@gmail.com>
2022-03-01 12:18:52 +0530

    Added : Find maximum in Float

--- src/com/blz/FindMaximumProblem.java
+++ src/com/blz/FindMaximumProblem.java
@@ -6,8 +6,8 @@ public class FindMaximumProblem {
 	 * Extend the max method to also print the max to std out using Generic Method
 	 */
 
-	public static Integer maxInteger(Integer a, Integer b, Integer c) {
-		Integer max = a;// assume a is initially largest
+	public static Float maxFloat(Float a, Float b, Float c) {
+		Float max = a;// assume a is initially largest
 		if (b.compareTo(max) > 0)
 			max = b;
 		if (c.compareTo(max) > 0)
@@ -23,7 +23,7 @@ public class FindMaximumProblem {
 	// main method
 	public static void main(String[] args) {
 		System.out.println("Welcome..!!");
-		// Calling method with Integer argument
-		FindMaximumProblem.maxInteger(7, 10, 9);
+		// Calling method with Float argument
+		FindMaximumProblem.maxFloat(7.12f, 10.32f, 9.45f);
 	}
 }
