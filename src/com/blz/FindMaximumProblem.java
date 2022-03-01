0fef235b3baf6d8e680877f6460349c53e314880
Priyanka <priyankadombale27@gmail.com>
2022-03-01 16:03:18 +0530

    Added : Find Maximum using Generic Method to refactor all 3

--- src/com/blz/FindMaximumProblem.java
+++ src/com/blz/FindMaximumProblem.java
@@ -1,29 +1,27 @@
 package com.blz;
 
 public class FindMaximumProblem {
-
 	/*
-	 * Extend the max method to also print the max to std out using Generic Method
+	 * This generic method is used to Refactor all the 3(Integer, float, String) to
+	 * One Generic Method and find the maximum value
 	 */
+	public static <T extends Comparable<T>> T maximum(T a, T b, T c) {
+		T max = a; // assume a is initially the largest
 
-	public static String maxString(String a, String b, String c) {
-		String max = a;// assume a is initially largest
-		if (b.compareTo(max) > 0)
+		if (b.compareTo(max) > 0) {
 			max = b;
-		if (c.compareTo(max) > 0)
+		}
+		if (c.compareTo(max) > 0) {
 			max = c;
-
-		System.out.println("First Number= " + a);
-		System.out.println("Second Number= " + b);
-		System.out.println("Third Number= " + c);
-		System.out.println("Maximum Value is= " + max);
-		return max;
+		}
+		return max; // returns the largest value
 	}
 
 	// main method
 	public static void main(String[] args) {
-		System.out.println("Welcome..!!");
-		// Calling method with String argument
-		FindMaximumProblem.maxString("Priya", "Anu", "Sonu");
+		// Calling method And passing values
+		System.out.printf("Max of %d, %d and %d is %d\n\n", 3, 4, 5, maximum(3, 4, 5));
+		System.out.printf("Max of %.1f,%.1f and %.1f is %.1f\n\n", 6.6, 8.8, 7.7, maximum(6.6f, 8.8f, 7.7f));
+		System.out.printf("Max of %s, %s and %s is %s\n", "Priya", "Anu", "Sonu", maximum("Priya", "Anu", "Sonu"));
 	}
 }
